9c7cf65683dff662eb2a66b24d937858249f5074
Evan <eulloyd@gmail.com>
2014-04-25 14:18:40 +0900

    Finish chapter 2, weird form issue on type/status

--- chapter-2/js/app.js
+++ chapter-2/js/app.js
@@ -1,22 +1,116 @@
 var myModule = angular.module('Angello', []);
 
-myModule.controller('MainCtrl', function($scope) {
-	$scope.statuses = [
-		{name: 'Backlog'},
-		{name: 'To do'},
-		{name: 'In progress'},
-		{name: 'Code Review'},
-		{name: 'QA Review'},
-		{name: 'Verified'},
-		{name: 'Done'}
-	];
+myModule.factory('angelloHelper', function($rootScope) {
+	var buildIndex = function(source, property) {
+		var tempArray = [];
 
-	$scope.types = [
-		{name: 'Feature'},
-		{name: 'Enhancement'},
-		{name: 'Bug'},
-		{name: 'Spike'}
-	];
+		for (var i = 0, len = source.length; i < len; i++) {
+			tempArray[source[i][property]] = source[i];
+		}
+
+		return tempArray;
+	};
+
+	return {
+		buildIndex: buildIndex
+	};
+});
+
+myModule.factory('angelloModel', function($rootScope) {
+	var getStatuses = function () {
+		var tempArray = [
+			{name: 'Backlog'},
+			{name: 'To do'},
+			{name: 'In progress'},
+			{name: 'Code Review'},
+			{name: 'QA Review'},
+			{name: 'Verified'},
+			{name: 'Done'}
+		];
+		return tempArray;
+	};
+
+	var getTypes = function() {
+		var tempArray = [
+			{name: 'Feature'},
+			{name: 'Enhancement'},
+			{name: 'Bug'},
+			{name: 'Spike'}
+		];
+		return tempArray;
+	};
+
+	var getStories = function() {
+		var tempArray = [
+			{
+				title: 'Story 00', description: 'Description pending.',
+				criteria: 'Criteria pending.', status: 'To do', type: 'Feature',
+				reporter: 'Evan', assignee: 'Obama'
+			},
+			{
+				title: 'Story 01', description: 'Description pending.',
+				criteria: 'Criteria pending.', status: 'To do', type: 'Feature',
+				reporter: 'Evan', assignee: 'Obama'
+			},
+			{
+				title: 'Story 02', description: 'Description pending.',
+				criteria: 'Criteria pending.', status: 'To do', type: 'Feature',
+				reporter: 'Evan', assignee: 'Obama'
+			},
+			{
+				title: 'Story 03', description: 'Description pending.',
+				criteria: 'Criteria pending.', status: 'To do', type: 'Feature',
+				reporter: 'Evan', assignee: 'Obama'
+			},
+			{
+				title: 'Story 04', description: 'Description pending.',
+				criteria: 'Criteria pending.', status: 'To do', type: 'Feature',
+				reporter: 'Evan', assignee: 'Obama'
+			},
+			{
+				title: 'Story 05', description: 'Description pending.',
+				criteria: 'Criteria pending.', status: 'Verified', type: 'Feature',
+				reporter: 'Evan', assignee: 'Obama'
+			},
+		];
+
+		return tempArray;
+	};
+
+	return {
+		getStatuses: getStatuses,
+		getTypes: getTypes,
+		getStories: getStories
+	};
+});
+
+myModule.controller('MainCtrl', function($scope, angelloModel, angelloHelper) {
+	$scope.currentStory;
+
+	$scope.types = angelloModel.getTypes();
+	$scope.statuses = angelloModel.getStatuses();
+	$scope.stories = angelloModel.getStories();
+	$scope.typesIndex = angelloHelper.buildIndex($scope.types, 'name');
+	$scope.statusesIndex = angelloHelper.buildIndex($scope.statuses, 'name');
+
+	$scope.setCurrentStory = function(story) {
+		$scope.currentStory = story;
+
+		$scope.currentStatus = $scope.statusesIndex[story.status];
+		$scope.currentType = $scope.typesIndex[story.type];
+	}
+
+	$scope.setCurrentStatus = function(status) {
+		if(typeof $scope.currentStory !== 'undefined') {
+			$scope.setCurrentStory.status = status.name;
+		}
+	};
+
+	$scope.setCurrentType = function(type) {
+		if(typeof $scope.currentStory !== 'undefined') {
+			$scope.setCurrentStory.type = type.name;
+		}
+	};
 
 	$scope.createStory = function () {
 		$scope.stories.push({
@@ -24,44 +118,4 @@ myModule.controller('MainCtrl', function($scope) {
 			description: 'Description pending.'
 		});
 	};
-
-	$scope.currentStory;
-
-	$scope.setCurrentStory = function(story) {
-		$scope.currentStory = story;
-		console.log($scope.currentStory)
-	};
-
-	$scope.stories = [
-		{
-			title: 'Story 00', description: 'Description pending.',
-			criteria: 'Criteria pending.', status: 'To Do', type: 'Feature',
-			reporter: 'Evan', assignee: 'Obama'
-		},
-		{
-			title: 'Story 01', description: 'Description pending.',
-			criteria: 'Criteria pending.', status: 'To Do', type: 'Feature',
-			reporter: 'Evan', assignee: 'Obama'
-		},
-		{
-			title: 'Story 02', description: 'Description pending.',
-			criteria: 'Criteria pending.', status: 'To Do', type: 'Feature',
-			reporter: 'Evan', assignee: 'Obama'
-		},
-		{
-			title: 'Story 03', description: 'Description pending.',
-			criteria: 'Criteria pending.', status: 'To Do', type: 'Feature',
-			reporter: 'Evan', assignee: 'Obama'
-		},
-		{
-			title: 'Story 04', description: 'Description pending.',
-			criteria: 'Criteria pending.', status: 'To Do', type: 'Feature',
-			reporter: 'Evan', assignee: 'Obama'
-		},
-		{
-			title: 'Story 05', description: 'Description pending.',
-			criteria: 'Criteria pending.', status: 'To Do', type: 'Feature',
-			reporter: 'Evan', assignee: 'Obama'
-		},
-	];
 });
