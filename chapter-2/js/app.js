cac1a8c41fdc53edea5bfd8d71f4d5ff1d72b6bd
Evan <eulloyd@gmail.com>
2014-04-25 11:17:35 +0900

    'p.26'

--- chapter-2/js/app.js
+++ chapter-2/js/app.js
@@ -1,6 +1,30 @@
 var myModule = angular.module('Angello', []);
 
 myModule.controller('MainCtrl', function($scope) {
+	$scope.statuses = [
+		{name: 'Backlog'},
+		{name: 'To do'},
+		{name: 'In progress'},
+		{name: 'Code Review'},
+		{name: 'QA Review'},
+		{name: 'Verified'},
+		{name: 'Done'}
+	];
+
+	$scope.types = [
+		{name: 'Feature'},
+		{name: 'Enhancement'},
+		{name: 'Bug'},
+		{name: 'Spike'}
+	];
+
+	$scope.createStory = function () {
+		$scope.stories.push({
+			title: 'New Story',
+			description: 'Description pending.'
+		});
+	};
+
 	$scope.currentStory;
 
 	$scope.setCurrentStory = function(story) {
@@ -9,11 +33,35 @@ myModule.controller('MainCtrl', function($scope) {
 	};
 
 	$scope.stories = [
-		{title: 'Story 00', description: 'Description pending.'},
-		{title: 'Story 01', description: 'Description pending.'},
-		{title: 'Story 02', description: 'Description pending.'},
-		{title: 'Story 03', description: 'Description pending.'},
-		{title: 'Story 04', description: 'Description pending.'},
-		{title: 'Story 05', description: 'Description pending.'},
+		{
+			title: 'Story 00', description: 'Description pending.',
+			criteria: 'Criteria pending.', status: 'To Do', type: 'Feature',
+			reporter: 'Evan', assignee: 'Obama'
+		},
+		{
+			title: 'Story 01', description: 'Description pending.',
+			criteria: 'Criteria pending.', status: 'To Do', type: 'Feature',
+			reporter: 'Evan', assignee: 'Obama'
+		},
+		{
+			title: 'Story 02', description: 'Description pending.',
+			criteria: 'Criteria pending.', status: 'To Do', type: 'Feature',
+			reporter: 'Evan', assignee: 'Obama'
+		},
+		{
+			title: 'Story 03', description: 'Description pending.',
+			criteria: 'Criteria pending.', status: 'To Do', type: 'Feature',
+			reporter: 'Evan', assignee: 'Obama'
+		},
+		{
+			title: 'Story 04', description: 'Description pending.',
+			criteria: 'Criteria pending.', status: 'To Do', type: 'Feature',
+			reporter: 'Evan', assignee: 'Obama'
+		},
+		{
+			title: 'Story 05', description: 'Description pending.',
+			criteria: 'Criteria pending.', status: 'To Do', type: 'Feature',
+			reporter: 'Evan', assignee: 'Obama'
+		},
 	];
 });
